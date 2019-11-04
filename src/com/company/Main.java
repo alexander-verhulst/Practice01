985e8a89ac19ed602b356e5f3b383056530d2814
Alexander William Verhulst <ftrr2017@gmail.com>
2019-11-04 13:32:18 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TestClass myObj = new TestClass();
+        System.out.println(myObj.number);
+
+    }
+}
